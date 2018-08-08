cfdff3b647624c0ea774091c95e20e433b232a9d
Hasif <hasif@annikken.com>
2018-08-08 16:39:50 +0800

    following template file structure for arduino libraries

--- src/EasyAndee101.h
+++ src/EasyAndee101.h
@@ -4,7 +4,7 @@ Author: Muhammad Hasif
 
 The Easy Andee Library is to allow users to use a feature similar to Arduino's Firmata to control their Arduino using their Bluetooth enabled phone. Users no longer need to code the UI and logic. With the Easy Andee feature on the Andee App, users can create their UI and immediately control their electronics.
 
-This library is free software. This library also uses parts of the Arduino Firmata Library */
+This library is free software*/
 
 #define BLE_LIMIT 18
 #define CONNECTION_ERROR 0
